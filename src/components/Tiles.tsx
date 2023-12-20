986ac48fd0bbed0e103caa80d0a7249ba1fb2c8b
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 11:23:46 -0500

    feat: refactor tiles to manage selection

--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type Item = {
   key: string;
   image: JSX.Element;
@@ -6,16 +8,34 @@ type Item = {
 
 interface TilesProps {
   items: Item[];
+  multipleSelection: boolean;
+  handleTileClick: (display: string) => void;
 }
-function Tiles({ items }: TilesProps) {
+
+function Tiles({ items, multipleSelection, handleTileClick }: TilesProps) {
+  const [selected, setSelected] = useState<string[] | string>(['']);
+
+  const handleClick = (display: string) => {
+    if (multipleSelection) {
+      const allSelected = [...selected, display];
+      setSelected(allSelected);
+    } else {
+      setSelected(display);
+    }
+    handleTileClick(display);
+  };
+
   return (
     <div className='grid grid-cols-2 md:grid-cols-3 gap-4 mt-8 content-center place-items-center md:w-[620px]'>
       {items &&
         items.map((current, index) => {
           return (
             <div
+              onClick={() => handleClick(current.display)}
               key={`${current.key}+${index}`}
-              className='flex flex-col p-4 border rounded-md text-center w-[200px] justify-center items-center h-[110px] hover:border-black'
+              className={`flex flex-col p-4 border rounded-md text-center w-[200px] justify-center items-center h-[110px] hover:border-black ${
+                selected.includes(current.display) ? 'border-black' : ''
+              }`}
             >
               <div className='pb-2'>{current.image}</div>
               <div>{current.display}</div>
