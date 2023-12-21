f58955df4068a62901bf93c774299ef4ed7f0042
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 21:55:18 -0500

    feat: create component for salmon color button

--- src/components/SalmonButton.tsx
+++ src/components/SalmonButton.tsx
@@ -2,13 +2,22 @@ interface SalmonButtonProps {
   display: string;
   handleClick: (operation?: string) => void;
   operation?: string;
+  disabled: boolean;
 }
 
-function SalmonButton({ display, handleClick, operation }: SalmonButtonProps) {
+function SalmonButton({
+  display,
+  handleClick,
+  operation,
+  disabled,
+}: SalmonButtonProps) {
   return (
     <button
+      disabled={disabled}
       onClick={() => handleClick(operation)}
-      className='rounded-full p-2 pl-8 pr-8 text-white bg-[var(--salmon)]'
+      className={`rounded-full p-2 pl-8 pr-8 text-white ${
+        disabled ? 'bg-gray-300' : 'bg-[var(--salmon)]'
+      } `}
     >
       {display}
     </button>
