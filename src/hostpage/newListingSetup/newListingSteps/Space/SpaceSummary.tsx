6e7e8cf49bad291b63f0d5528ccb8275b5e08181
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 22:01:03 -0500

    feat: update spacesummary to use new buttons and context

--- src/hostpage/newListingSetup/newListingSteps/Space/SpaceSummary.tsx
+++ src/hostpage/newListingSetup/newListingSteps/Space/SpaceSummary.tsx
@@ -1,16 +1,47 @@
 import Homes from '../../../../assets/Homes.png';
 import NewListingWizardSummary from '../../../../components/NewListingWizardSummary';
+import SalmonButton from '../../../../components/SalmonButton';
+import { useContext } from 'react';
+import { NewListingWizardContext } from '../../NewListingWizard';
+
 function SpaceSummary() {
+  const { newListingButtonsContext } = useContext(NewListingWizardContext);
+  const { currentView, setCurrentView } = newListingButtonsContext;
+
+  const handleView = (operation?: string) => {
+    if (operation === 'Forward') {
+      setCurrentView(currentView + 1);
+    } else if (operation === 'Backward') setCurrentView(currentView - 1);
+  };
+
   return (
-    <div>
-      <NewListingWizardSummary
-        header={'Tell us about your place'}
-        description={
-          "In this step, we'll ask you which type of property you have and if guests will book the entire place or just a specific room. Then let us know the location and how many guests can join."
-        }
-        image={Homes}
-        step={1}
-      />
+    <div className='flex flex-col h-full overflow-auto'>
+      <div className='mt-10 md:mt-auto'>
+        <NewListingWizardSummary
+          header={'Tell us about your place'}
+          description={
+            "In this step, we'll ask you which type of property you have and if guests will book the entire place or just a specific room. Then let us know the location and how many guests can join."
+          }
+          image={Homes}
+          step={1}
+        />
+      </div>
+      <div className='flex justify-center mb-8 mt-auto pt-6'>
+        <div className='flex w-full justify-between ml-4 mr-4'>
+          <SalmonButton
+            display={'Back'}
+            handleClick={handleView}
+            operation={'Backward'}
+            disabled={false}
+          />
+          <SalmonButton
+            display={'Next'}
+            handleClick={handleView}
+            operation={'Forward'}
+            disabled={false}
+          />
+        </div>
+      </div>
     </div>
   );
 }
