17f3c9a21029bb486e485328ad00acbaf0f3330b
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 21:57:08 -0500

    feat: update addtitle to persist state using context

--- src/hostpage/newListingSetup/newListingSteps/Marketing/AddTitle.tsx
+++ src/hostpage/newListingSetup/newListingSteps/Marketing/AddTitle.tsx
@@ -1,25 +1,83 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { NewListingWizardContext } from '../../NewListingWizard';
+import SalmonButton from '../../../../components/SalmonButton';
+
 /*
 TODO: Add logic for erroring when longer than 32 chars 
 */
+
 function AddTitle() {
-  const [charsLength, setCharsLength] = useState<number>(0);
+  const { marketingContext, newListingButtonsContext } = useContext(
+    NewListingWizardContext
+  );
+  const { marketingDetails, setMarketingDetails } = marketingContext;
+  const { currentView, setCurrentView } = newListingButtonsContext;
+  const [notValidated, setNotValidated] = useState<boolean>(true);
+
+  const handleTitleUpdate = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const title = e.target.value;
+    setMarketingDetails({ ...marketingDetails, title });
+  };
+
+  useEffect(() => {
+    if (marketingDetails.title && marketingDetails.title.length <= 32)
+      setNotValidated(false);
+    else setNotValidated(true);
+  }, [marketingDetails]);
+
+  const handleView = (operation?: string) => {
+    localStorage.setItem('marketingDetails', JSON.stringify(marketingDetails));
+    if (operation === 'Forward') {
+      setCurrentView(currentView + 1);
+    } else if (operation === 'Backward') setCurrentView(currentView - 1);
+  };
 
   return (
-    <div className='flex flex-col'>
-      <div className='font-black text-2xl md:text-3xl pt-10 md:pt-0'>
-        Now, let's give your house a title
+    <div className='flex flex-col h-full overflow-auto'>
+      <div className='flex flex-col md:mt-auto mx-auto'>
+        <div className='font-black text-2xl md:text-3xl pt-10 md:pt-0'>
+          Now, let's give your house a title
+        </div>
+        <div className='mt-4 md:text-base text-gray-500'>
+          Short titles work best. Have fun with it—you can always change it
+          later.
+        </div>
+        <textarea
+          className={`mt-8 border rounded-md p-2 whitespace-normal h-20 ${
+            marketingDetails.title.length > 32
+              ? 'outline-rose-800 bg-rose-800/20'
+              : 'outline-[var(--light-pink)]'
+          }`}
+          id='title'
+          onChange={handleTitleUpdate}
+        ></textarea>
+        <label htmlFor='title'></label>
+        <div
+          className={`font-black mt-3 ${
+            marketingDetails.title.length > 32
+              ? 'text-rose-800'
+              : 'text-gray-500'
+          }`}
+        >
+          {marketingDetails.title.length}/32
+        </div>
       </div>
-      <div className='mt-4 md:text-base text-gray-500'>
-        Short titles work best. Have fun with it—you can always change it later.
+      <div className='flex justify-center mb-8 mt-auto pt-6'>
+        <div className='flex w-full justify-between ml-4 mr-4'>
+          <SalmonButton
+            display={'Back'}
+            handleClick={handleView}
+            operation={'Backward'}
+            disabled={false}
+          />
+          <SalmonButton
+            display={'Next'}
+            handleClick={handleView}
+            operation={'Forward'}
+            disabled={notValidated}
+          />
+        </div>
       </div>
-      <textarea
-        className='mt-8 border rounded-md outline-[var(--light-pink)] p-2 whitespace-normal h-20'
-        id='title'
-        onChange={(e) => setCharsLength(e.target.value.length)}
-      ></textarea>
-      <label htmlFor='title'></label>
-      <div className='text-gray-500 font-black mt-3'>{charsLength}/32</div>
     </div>
   );
 }
