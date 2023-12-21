a3c23bb3cd3f945215c3443e943e00cfe55626e3
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 22:02:05 -0500

    feat: update homeaccess to persist state using context

--- src/hostpage/newListingSetup/newListingSteps/Space/HomeAccess.tsx
+++ src/hostpage/newListingSetup/newListingSteps/Space/HomeAccess.tsx
@@ -2,17 +2,27 @@ import { MdOutlineMeetingRoom } from 'react-icons/md';
 import { BsHouseDoor } from 'react-icons/bs';
 import SelectableCards from '../../../../components/SelectableCards';
 import { NewListingWizardContext } from '../../NewListingWizard';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import SalmonButton from '../../../../components/SalmonButton';
 
 function HomeAccess() {
-  const { spaceContext } = useContext(NewListingWizardContext);
+  const { spaceContext, newListingButtonsContext } = useContext(
+    NewListingWizardContext
+  );
   const { spaceDetails, setSpaceDetails } = spaceContext;
+  const { currentView, setCurrentView } = newListingButtonsContext;
+  const [notValidated, setNotValidated] = useState<boolean>(true);
 
   const updateHomeAccess = (accessType: string) => {
-    const currentSpaceDetails = spaceDetails;
-    currentSpaceDetails.accessType = accessType;
-    setSpaceDetails(currentSpaceDetails);
-    localStorage.setItem('spaceDetails', JSON.stringify(currentSpaceDetails));
+    setSpaceDetails({ ...spaceDetails, accessType });
+    setNotValidated(false);
+  };
+
+  const handleView = (operation?: string) => {
+    localStorage.setItem('spaceDetails', JSON.stringify(spaceDetails));
+    if (operation === 'Forward') {
+      setCurrentView(currentView + 1);
+    } else if (operation === 'Backward') setCurrentView(currentView - 1);
   };
 
   const accessTypes = [
@@ -31,16 +41,34 @@ function HomeAccess() {
     },
   ];
   return (
-    <div className='flex flex-col md:space-x-8'>
-      <div className='flex flex-col pb-10 pt-10 md:pb-0 md:p-10'>
-        <div className='font-black text-2xl md:text-3xl'>
-          What space will guests have access to?
+    <div className='flex flex-col h-full overflow-auto'>
+      <div className='flex flex-col md:space-x-8 md:mt-auto justify-center items-center'>
+        <div className='flex flex-col pb-10 pt-10 md:pb-0 md:p-10 md:w-[700px] '>
+          <div className='font-black text-2xl md:text-3xl'>
+            What space will guests have access to?
+          </div>
+          <SelectableCards
+            cards={accessTypes}
+            multipleSelection={false}
+            handleSelectableCardClick={updateHomeAccess}
+          />
         </div>
-        <SelectableCards
-          cards={accessTypes}
-          multipleSelection={false}
-          handleSelectableCardClick={updateHomeAccess}
-        />
+      </div>
+      <div className='flex justify-center mb-8 mt-auto pt-6'>
+        <div className='flex w-full justify-between ml-4 mr-4'>
+          <SalmonButton
+            display={'Back'}
+            handleClick={handleView}
+            operation={'Backward'}
+            disabled={false}
+          />
+          <SalmonButton
+            display={'Next'}
+            handleClick={handleView}
+            operation={'Forward'}
+            disabled={notValidated}
+          />
+        </div>
       </div>
     </div>
   );
