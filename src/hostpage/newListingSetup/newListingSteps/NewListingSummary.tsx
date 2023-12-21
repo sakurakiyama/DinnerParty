a54bc9c1f1f25edb5c893d892437f91353b04d7a
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 22:00:51 -0500

    feat: update newlistingsummary to use new buttons and context

--- src/hostpage/newListingSetup/newListingSteps/NewListingSummary.tsx
+++ src/hostpage/newListingSetup/newListingSteps/NewListingSummary.tsx
@@ -1,11 +1,21 @@
 import Plate from '../../../assets/Plate.png';
 import WinePour from '../../../assets/WinePour.png';
 import HangingLights from '../../../assets/HangingLights.png';
+import SalmonButton from '../../../components/SalmonButton';
+import { NewListingWizardContext } from '../NewListingWizard';
+import { useContext } from 'react';
 
 /*
 TODO: Generate unique key other than index for map. 
 */
 function NewListingSummary() {
+  const { newListingButtonsContext } = useContext(NewListingWizardContext);
+  const { currentView, setCurrentView } = newListingButtonsContext;
+
+  const handleView = () => {
+    setCurrentView(currentView + 1);
+  };
+
   const sections = [
     {
       number: 1,
@@ -31,30 +41,49 @@ function NewListingSummary() {
   ];
 
   return (
-    <div className='flex flex-col md:flex-row md:space-x-8 items-center'>
-      <div className='font-black text-2xl md:text-4xl md:w-[50%] flex items-center justify-center pb-10 pt-10 md:pb-0 md:p-10'>
-        It's easy to start hosting a listing on Dinner Party
+    <div className='flex flex-col h-full overflow-auto'>
+      <div className='md:m-auto'>
+        <div className='m-8 flex justify-center items-center'>
+          <div className='flex flex-col md:flex-row md:space-x-8 items-center'>
+            <div className='font-black text-2xl md:text-4xl md:w-[50%] flex items-center justify-center pb-10 pt-10 md:pb-0 md:p-10'>
+              It's easy to start hosting a listing on Dinner Party
+            </div>
+            <div className='md:w-[50%] space-y-8'>
+              {sections &&
+                sections.map((current, index) => {
+                  return (
+                    <div className='flex flex-row items-center' key={index}>
+                      <div>
+                        <div className='text-lg font-black flex'>
+                          <div>{current.number}</div>
+                          <div className='pl-4 font-bold'>
+                            {current.heading}
+                          </div>
+                        </div>
+                        <div className='pl-6 text-gray-500 mt-2'>
+                          {current.caption}
+                        </div>
+                      </div>
+                      <div className='ml-auto'>
+                        <img
+                          className='w-[125px] h-auto'
+                          src={current.image}
+                        ></img>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='md:w-[50%] space-y-8'>
-        {sections &&
-          sections.map((current, index) => {
-            return (
-              <div className='flex flex-row items-center' key={index}>
-                <div>
-                  <div className='text-lg font-black flex'>
-                    <div>{current.number}</div>
-                    <div className='pl-4 font-bold'>{current.heading}</div>
-                  </div>
-                  <div className='pl-6 text-gray-500 mt-2'>
-                    {current.caption}
-                  </div>
-                </div>
-                <div className='ml-auto'>
-                  <img className='w-[125px] h-auto' src={current.image}></img>
-                </div>
-              </div>
-            );
-          })}
+      <div className='flex justify-center mb-8 mt-auto'>
+        <SalmonButton
+          display={'Get started'}
+          handleClick={handleView}
+          operation={'Forward'}
+          disabled={false}
+        />
       </div>
     </div>
   );
