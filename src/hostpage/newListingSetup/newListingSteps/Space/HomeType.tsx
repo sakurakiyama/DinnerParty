f01c8c5a31ba84903ddccddecba4cc56b907182d
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 21:59:44 -0500

    feat: update hometype to persist state using context

--- src/hostpage/newListingSetup/newListingSteps/Space/HomeType.tsx
+++ src/hostpage/newListingSetup/newListingSteps/Space/HomeType.tsx
@@ -11,17 +11,26 @@ import { IoBedOutline } from 'react-icons/io5';
 import { LuHotel } from 'react-icons/lu';
 import Tiles from '../../../../components/Tiles';
 import { NewListingWizardContext } from '../../NewListingWizard';
-import { useContext } from 'react';
-
+import { useContext, useState } from 'react';
+import SalmonButton from '../../../../components/SalmonButton';
 function HomeType() {
-  const { spaceContext } = useContext(NewListingWizardContext);
+  const { spaceContext, newListingButtonsContext } = useContext(
+    NewListingWizardContext
+  );
   const { spaceDetails, setSpaceDetails } = spaceContext;
+  const { currentView, setCurrentView } = newListingButtonsContext;
+  const [notValidated, setNotValidated] = useState<boolean>(true);
 
   const updateHomeType = (homeType: string) => {
-    const currentSpaceDetails = spaceDetails;
-    currentSpaceDetails.homeType = homeType;
-    setSpaceDetails(currentSpaceDetails);
-    localStorage.setItem('spaceDetails', JSON.stringify(currentSpaceDetails));
+    setSpaceDetails({ ...spaceDetails, homeType });
+    setNotValidated(false);
+  };
+
+  const handleView = (operation?: string) => {
+    localStorage.setItem('spaceDetails', JSON.stringify(spaceDetails));
+    if (operation === 'Forward') {
+      setCurrentView(currentView + 1);
+    } else if (operation === 'Backward') setCurrentView(currentView - 1);
   };
 
   const allHomes = [
@@ -83,16 +92,34 @@ function HomeType() {
   ];
 
   return (
-    <div className='flex flex-col md:space-x-8 items-center justify-center '>
-      <div className='flex flex-col pb-10 pt-10 md:pb-0 md:p-10 items-center'>
-        <div className='font-black text-2xl md:text-3xl text-center'>
-          Which of these best describes your place?
+    <div className='flex flex-col h-full overflow-auto'>
+      <div className='flex flex-col md:space-x-8 items-center justify-center md:mt-auto'>
+        <div className='flex flex-col pb-10 pt-10 md:pb-0 md:p-10 items-center'>
+          <div className='font-black text-2xl md:text-3xl text-center'>
+            Which of these best describes your place?
+          </div>
+          <Tiles
+            items={allHomes}
+            multipleSelection={false}
+            handleTileClick={updateHomeType}
+          />
         </div>
-        <Tiles
-          items={allHomes}
-          multipleSelection={false}
-          handleTileClick={updateHomeType}
-        />
+      </div>
+      <div className='flex justify-center mb-8 mt-auto pt-6'>
+        <div className='flex w-full justify-between ml-4 mr-4'>
+          <SalmonButton
+            display={'Back'}
+            handleClick={handleView}
+            operation={'Backward'}
+            disabled={false}
+          />
+          <SalmonButton
+            display={'Next'}
+            handleClick={handleView}
+            operation={'Forward'}
+            disabled={notValidated}
+          />
+        </div>
       </div>
     </div>
   );
