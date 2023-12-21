c91875e531a21efc05d4d955ba66c33b3fb80d81
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 21:58:09 -0500

    feat: update instantbookaccess to persist state using context

--- src/hostpage/newListingSetup/newListingSteps/publish/InsantBookAccess.tsx
+++ src/hostpage/newListingSetup/newListingSteps/publish/InsantBookAccess.tsx
@@ -1,7 +1,36 @@
 import { RxLightningBolt } from 'react-icons/rx';
 import { PiChats } from 'react-icons/pi';
 import SelectableCards from '../../../../components/SelectableCards';
+import { NewListingWizardContext } from '../../NewListingWizard';
+import { useContext, useState } from 'react';
+import SalmonButton from '../../../../components/SalmonButton';
 function InstantBookAccess() {
+  const { publishingContext, newListingButtonsContext } = useContext(
+    NewListingWizardContext
+  );
+  const { publishingDetails, setPublishingDetails } = publishingContext;
+  const { currentView, setCurrentView } = newListingButtonsContext;
+  const [notValidated, setNotValidated] = useState<boolean>(true);
+
+  const updateBookingType = (bookingType: string) => {
+    if (bookingType === 'Use instant book')
+      setPublishingDetails({ ...publishingDetails, instantBook: true });
+    else if (bookingType === 'Approve or decline requests')
+      setPublishingDetails({ ...publishingDetails, instantBook: false });
+
+    setNotValidated(false);
+  };
+
+  const handleView = (operation?: string) => {
+    localStorage.setItem(
+      'publishingDetails',
+      JSON.stringify(publishingDetails)
+    );
+    if (operation === 'Forward') {
+      setCurrentView(currentView + 1);
+    } else if (operation === 'Backward') setCurrentView(currentView - 1);
+  };
+
   const accessTypes = [
     {
       key: 'instantbook',
@@ -17,12 +46,34 @@ function InstantBookAccess() {
     },
   ];
   return (
-    <div className='flex flex-col md:space-x-8'>
-      <div className='flex flex-col pt-10 md:pt-0 '>
-        <div className='font-black text-2xl md:text-3xl'>
-          Decide how you’ll confirm reservations
+    <div className='flex flex-col h-full overflow-auto'>
+      <div className='flex flex-col md:space-x-8 md:mt-auto justify-center items-center'>
+        <div className='flex flex-col pt-10 md:pt-0 '>
+          <div className='font-black text-2xl md:text-3xl'>
+            Decide how you’ll confirm reservations
+          </div>
+          <SelectableCards
+            cards={accessTypes}
+            multipleSelection={false}
+            handleSelectableCardClick={updateBookingType}
+          />
         </div>
-        <SelectableCards cards={accessTypes} />
+      </div>
+      <div className='flex justify-center mb-8 mt-auto pt-6'>
+        <div className='flex w-full justify-between ml-4 mr-4'>
+          <SalmonButton
+            display={'Back'}
+            handleClick={handleView}
+            operation={'Backward'}
+            disabled={false}
+          />
+          <SalmonButton
+            display={'Next'}
+            handleClick={handleView}
+            operation={'Forward'}
+            disabled={notValidated}
+          />
+        </div>
       </div>
     </div>
   );
