ecf7f6fff5e56a689c11c8831c3e93016dad0070
sakurakiyama <sakura.bowden@gmail.com>
2023-12-20 22:01:14 -0500

    feat: update reviewsummary to use new buttons and context

--- src/hostpage/newListingSetup/ReviewSummary.tsx
+++ src/hostpage/newListingSetup/ReviewSummary.tsx
@@ -1,8 +1,20 @@
 import { GoChecklist } from 'react-icons/go';
 import { LuCalendarHeart } from 'react-icons/lu';
 import { MdOutlineModeEditOutline } from 'react-icons/md';
+import SalmonButton from '../../components/SalmonButton';
+import { useContext } from 'react';
+import { NewListingWizardContext } from './NewListingWizard';
 
 function ReviewSummary() {
+  const { newListingButtonsContext } = useContext(NewListingWizardContext);
+  const { currentView, setCurrentView, saveListing } = newListingButtonsContext;
+
+  const handleView = (operation?: string) => {
+    if (operation === 'Forward') {
+      setCurrentView(currentView + 1);
+    } else if (operation === 'Backward') setCurrentView(currentView - 1);
+  };
+
   const nextSteps = [
     {
       image: <GoChecklist size={30} />,
@@ -28,35 +40,55 @@ function ReviewSummary() {
   ];
 
   return (
-    <div className='flex flex-col pt-10 md:pt-0 w-[800px]'>
-      <div className='font-black text-2xl md:text-3xl'>Review your listing</div>
-      <div className='mt-4 md:text-base text-gray-500'>
-        Here's what we'll show to guests. Make sure everything looks good.
-      </div>
-      <div className='flex flex-col md:flex-row w-full pt-8'>
-        <div className='m-4 flex md:w-[50%] border p-4 justify-center items-center'>
-          This is where the preview will go
+    <div className='flex flex-col h-full overflow-auto'>
+      <div className='flex flex-col pt-10 md:pt-0 md:w-[800px] md:mt-auto md:mx-auto'>
+        <div className='font-black text-2xl md:text-3xl'>
+          Review your listing
         </div>
-        <div className='m-4 md:w-[50%] p-4 justify-center items-center space-y-6'>
-          {nextSteps &&
-            nextSteps.map((current, index) => {
-              return (
-                <div
-                  key={`${current.key}+${index}`}
-                  className='flex flex-row space-x-4'
-                >
-                  <div>{current.image}</div>
-                  <div>
-                    <div className='font-black text-base pb-1'>
-                      {current.title}
-                    </div>
-                    <div className='text-xs text-gray-500'>
-                      {current.description}
+        <div className='mt-4 md:text-base text-gray-500'>
+          Here's what we'll show to guests. Make sure everything looks good.
+        </div>
+        <div className='flex flex-col md:flex-row w-full pt-8'>
+          <div className='m-4 flex md:w-[50%] border p-4 justify-center items-center'>
+            This is where the preview will go
+          </div>
+          <div className='m-4 md:w-[50%] p-4 justify-center items-center space-y-6'>
+            {nextSteps &&
+              nextSteps.map((current, index) => {
+                return (
+                  <div
+                    key={`${current.key}+${index}`}
+                    className='flex flex-row space-x-4'
+                  >
+                    <div>{current.image}</div>
+                    <div>
+                      <div className='font-black text-base pb-1'>
+                        {current.title}
+                      </div>
+                      <div className='text-xs text-gray-500'>
+                        {current.description}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+          </div>
+        </div>
+      </div>
+      <div className='flex justify-center mb-8 mt-auto pt-6'>
+        <div className='flex w-full justify-between ml-4 mr-4'>
+          <SalmonButton
+            display={'Back'}
+            handleClick={handleView}
+            operation={'Backward'}
+            disabled={false}
+          />
+          <SalmonButton
+            display={"Let's go"}
+            handleClick={saveListing}
+            operation={'Forward'}
+            disabled={false}
+          />
         </div>
       </div>
     </div>
